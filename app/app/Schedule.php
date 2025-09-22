41c7573a7e1bb22a87045b22b29de4faea41dff4
iwadate-asuka <144047505+iwadate-asuka@users.noreply.github.com>
2025-09-22 22:47:16 +0900

    20250922-予定追加機能-実装中

--- app/app/Schedule.php
+++ app/app/Schedule.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Schedule extends Model
 {
     //
@@ -11,8 +12,11 @@ class Schedule extends Model
         return $this->belongsTo(ScheduleCategory::class,'categoy_id','id');
     }
 
+    protected $fillable = ['calendar_id','title','start_date','start_time','end_date','end_time','all_day','category_id','place_name','place_adress','latitude','longitude','comment','creator_id','del_flg'];
+
     // 1つのカレンダーに属する
-    public function calendar(){
-         return $this->belongsTo(Calendar::class,'caledar_id','id');
+    public function calendars(){
+        return $this->belongsTo(Calendar::class,'caledar_id','id');
     }
+
 }
